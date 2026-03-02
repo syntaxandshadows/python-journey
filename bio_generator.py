daccccbeca8a33bdfcd7a13768012ce72644922f
Kira C <socialbykira@gmail.com>
2026-03-02 09:46:07 -0500

    Add bio_generator.py script
    This script uses input functions, variables, and f-strings to create a user profile.

--- bio_generator.py
+++ bio_generator.py
@@ -1,4 +1,4 @@
 name = input("What's your name? ")
 color = input("What's your favorite color? ")
 
-print(f"My name is {name}, and my favorite color is {color}.")
+print(f" My name is {name}, and my favorite color is {color}.")
