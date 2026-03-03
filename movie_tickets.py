3b098792cdbf2f73a28f2f3dbd5050e019b81087
Kira C <socialbykira@gmail.com>
2026-03-03 12:00:55 -0500

    Update movie_tickets.py

--- movie_tickets.py
+++ movie_tickets.py
@@ -1,10 +1,10 @@
 if age < 13:
     price = 8
-elif age 13 >= 64:
+elif age >= 13 and < 64:
     price = 12
 else:
     price = 10
 
 name = input("Whats your name? ")
 movie = input("Whats the movies name? ")
-age = int(input("How old are you? ")
+age = int(input("How old are you? "))
