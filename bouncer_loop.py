21806eab39a843d567916fcbe9f1d4911a3b5b05
Kira C <socialbykira@gmail.com>
2026-03-11 11:42:54 -0400

    Enhance bouncer loop functionality and messages
    Refine loop exit condition and improve farewell message.

--- bouncer_loop.py
+++ bouncer_loop.py
@@ -1,11 +1,12 @@
 active = True
 
-while active == True:
+while active == True: # This means "Loop forever"
   name = input("What's your name? (or type 'quit'): ")
 
 if name == "quit":
-  active = False
-  print("Goodbye")
+  print("Goodbye!")
+  break  # This kills the loop
+  
 else:
   age = int(input("What's your age?"))
   if age >= 21:
