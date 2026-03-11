03a2efe544b910c5a51d6413850819de237d5bde
Kira C <socialbykira@gmail.com>
2026-03-11 11:31:25 -0400

    Project: Persistent Bouncer using nested logic and while loops.
    Added user age check and quit option to the loop.

--- bouncer_loop.py
+++ bouncer_loop.py
@@ -1,6 +1,14 @@
 active = True
 
 while active == True:
-  input("What's your name?")
-if active == False:
-  break
+  name = input("What's your name? (or type 'quit'): ")
+
+if name == "quit":
+  active = False
+  print("Goodbye")
+else:
+  age = int(input("What's your age?"))
+  if age >= 21:
+      print(f"Welcome to the club, {name}!"
+  else:
+      print(f"Sorry {name}, you are too young.")
